feb6cc9a54270b402b91f28239b8e9c5412182c3
Joan Ojukwu <joan.ojukwu11@gmail.com>
2024-04-08 23:01:09 -0400

    template + demo methods

---
+++ demo.py
@@ -0,0 +1,37 @@
+class Book:
+    def __init__(self, title, author, genre, copies):
+        pass
+    
+    def add_to_waitlist(self, customer):
+        pass
+    
+    def remove_from_waitlist(self):
+        pass;
+    
+class Customer:
+    def __init__(self, first_name, last_name, library_card):
+        pass
+    
+class Library:
+    def __init__(self):
+        pass
+    
+    def add_customer(self, customer):
+       pass
+
+    def remove_customer(self, customer):
+        pass
+    
+    def checkout_book(self, book_title, customer_card):
+        pass
+    
+
+    def return_book(self, book_title, customer_card):
+        pass
+    
+    def calculate_late_fees(self, book_title):
+        pass
+    
+    
+if __name__ == "__main__":
+    pass
