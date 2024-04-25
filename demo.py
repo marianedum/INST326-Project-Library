89600545e13695740a8c227d31adeadf3c865d4a
Joan Ojukwu <joan.ojukwu11@gmail.com>
2024-04-24 23:02:15 -0400

    typo

--- demo.py
+++ demo.py
@@ -1,5 +1,3 @@
-import sys
-
 class Book:
     def __init__(self, title, author, genre, copies):
         pass
@@ -56,31 +54,17 @@ class Library:
     
     def remove_customer(self, id): 
         for email, customer in self.customers.items():
-            if customer['id'] ==. id:
+            if customer['id'] == id:
                 del self.customers[email]
                 return True
             return False
     
-    
     def checkout_book(self, book_title, customer_card):
-        with open('library.txt', 'r') as file:
-                books = file.readlines()
-                books = [book.strip()for book in books]
-        
-        if book_title in books:
-                return f"Book {book_title} checkout initiated for {customer_card} Please return the {book_title} in 14 days."
-        else:
-                return f"Book {book_title} not found in library."
-        
+        pass
+    
+
     def return_book(self, book_title, customer_card):
-        if book_title in self.checked_out_book:
-                if self.checked_out_book[book_title] == customer.card:
-                    del self.checked_out_book[book_title]
-                    return f"Book {book_title} has been returned"
-                else:
-                    return f"This book was not checked out by {customer_card}"
-        else:
-            return f"This book is not checked out or was not checked out."
+        pass
     
     def calculate_late_fees(self, book_title):
         pass
